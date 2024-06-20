627d88b2e4a6d491c1cd52e8bca9e775aab78042
Juan029 <juadcobo1@misena.edu.co>
2024-06-20 16:34:01 -0500

    Cambios en el responsive

--- js/script.js
+++ js/script.js
@@ -2,4 +2,16 @@ const header = document.querySelector('header')
 
 window.addEventListener('scroll', () => {
     header.classList.toggle('sticky', this.window.scrollY > 50)
-})
+});
+
+
+//open menu
+
+
+let menu = document.querySelector('#iconoMenu');
+let navlist = document.querySelector('.navlist');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('open')
+}
